86594377cd7d4bf84b3c9b3f5131a894c8fafefd
Raphaël Hertzog <hertzog@debian.org>
2013-08-21 14:02:16 +0200

    Create /etc/galette/local_paths.inc.php
    * Configure galette to use FHS compliant paths.
    * Replace 4 embedded libraries by their packaged versions.

---
+++ debian/local_paths.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+// Default paths used by the Debian package
+define('GALETTE_CONFIG_PATH', '/etc/galette/');
+define('GALETTE_LOGS_PATH', '/var/log/galette/');
+define('GALETTE_COMPILE_DIR', '/var/cache/galette/templates_c/');
+define('GALETTE_CACHE_DIR', '/var/cache/galette/cache/');
+define('GALETTE_EXPORTS_PATH', '/var/lib/galette/exports/');
+define('GALETTE_PHOTOS_PATH', '/var/lib/galette/photos/');
+define('GALETTE_TEMPIMAGES_PATH', '/var/cache/galette/tempimages/');
+define('GALETTE_SQLITE_PATH', '/var/lib/galette/database.sqlite');
+
+// External libraries packaged separately
+define('GALETTE_ZEND_PATH', '/usr/share/php/');
+define('GALETTE_PHP_MAILER_PATH', '/usr/share/php/libphp-phpmailer/');
+define('GALETTE_SMARTY_PATH', '/usr/share/php/smarty3/');
+define('GALETTE_TCPDF_PATH', '/usr/share/php/tcpdf/';
+
+//Analog is not packaged in Debian (as of 20130821)
+//define('GALETTE_ANALOG_PATH', '/usr/share/php/');
+
+//Google-api is not packaged in Debian (as of 20130821)
+//define('GALETTE_GAPI_PATH', '/usr/share/php/');
+
+?>
