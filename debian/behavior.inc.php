0da374f1f54b9816e7fcc857c6dd5a20b34f38ca
Raphaël Hertzog <hertzog@debian.org>
2013-08-21 11:16:01 +0200

    Drop GALETTE_UNSECURE_PASSWORDS and increase dependencies to require Wheezy's PHP.

--- debian/behavior.inc.php
+++ debian/behavior.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-// Squeeze doesn't have the required support in php-bcrypt so keep using
-// md5 hashes for now
-define('GALETTE_UNSECURE_PASSWORDS', true);
+// If you backport this package for Squeeze, you have to enable this
+// option because Squeeze doesn't have the required support in php-bcrypt
+// define('GALETTE_UNSECURE_PASSWORDS', true);
 
 // Uncomment if you need debug logs (default level is 5)
 // define('GALETTE_LOG_LVL', 10);
